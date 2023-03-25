e626bc1af579cf0c1fbb221c3328b0038141119b
Dasha <lutaydaria@gmail.com>
2023-03-25 10:46:48 +0300

    All

---
+++ tests/test_check_icon_demoqa.py
@@ -0,0 +1,12 @@
+from pages.demoqa import DemoQa
+
+
+def test_icon_exist(browser):
+    demo_qa_page = DemoQa(browser)
+    demo_qa_page.visit()
+    demo_qa_page.icon.click()
+    assert demo_qa_page.exist_icon()
+    assert demo_qa_page.equal_url()
+
+
+
---
+++ tests/test_visible_btn_sidebar().py
@@ -0,0 +1,13 @@
+from pages.elements_page import ElementsPage
+# import time
+
+
+def test_visible_btn_sidebar(browser):
+    elements_page = ElementsPage(browser)
+
+    elements_page.visit()
+    # elements_page.btn_sidebar_first.click()
+    # time.sleep(3)
+
+    # assert elements_page.btn_sidebar_first_textbox.exist()
+    assert elements_page.btn_sidebar_first_textbox.visible()
