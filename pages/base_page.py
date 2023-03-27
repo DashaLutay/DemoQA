df0978eab26debdc4ec2cf4dc9faef14520fa858
Dasha <lutaydaria@gmail.com>
2023-03-27 19:24:11 +0300

    All 2

--- pages/base_page.py
+++ pages/base_page.py
@@ -17,5 +17,15 @@ class BasePage:
     def get_url(self):
         return self.driver.current_url
 
+    def back(self):
+        self.driver.back()
 
+    def forward(self):
+        self.driver.forward()
 
+    def refresh(self):
+        self.driver.refresh()
+
+    def get_title(self):
+        return self.driver.title
+    
